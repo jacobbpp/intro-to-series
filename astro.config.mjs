b568b4e8f04003a7a834d279387b4d681749f134
Jacob Reilly-Cooper <jacobreillycooper@bpp.com>
2025-07-15 13:59:49 +0100

    We've changed a lot. Let's see if I messed it up.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://jacobbpp.github.io/intro-to-series/',
-  base: '/intro-to-series/',
-  integrations: [mdx(), sitemap()],
+	site: 'https://jacobbpp.github.io/intro-to-series',
+	base: '/intro-to-series',
+	integrations: [mdx(), sitemap()],
 });
