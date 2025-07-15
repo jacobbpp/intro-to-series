86b0e608ed27ecc1fd41358e9b670a51e06070e4
Jacob Reilly-Cooper <jacobreillycooper@bpp.com>
2025-07-15 12:39:22 +0100

    astro config setup for Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://jacobbpp.github.io/Intro-To-Series/',
+  base: '/Intro-To-Series/',
+  integrations: [mdx(), sitemap()],
 });
